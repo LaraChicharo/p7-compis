01878caf3976d38e66754e3246b9a447b75de63f
Adrián García Pérez <adrian.garcia04@ciencias.unam.mx>
2021-01-12 19:59:23 -0600

    Añadiendo funciones al parser

--- p7/src/parser.c
+++ p7/src/parser.c
@@ -54,7 +54,7 @@ void C(){
   if (equals(tokenActual, CIZQ)) {
     char* valor;
     eat(CIZQ);
-    valor = tokenActual.valor();
+    valor = tokenActual.valor;
     eat(NUM);
     eat(CDER);
     C();
@@ -62,35 +62,84 @@ void C(){
 }
 
 void L(){
-
+  if (equals(tokenActual, ID)) {
+    eat(ID);
+    LP();
+  }
 }
 
 void LP(){
-
+  if (equals(tokenActual, COMA)) {
+    eat(COMA);
+    eat(ID);
+    LP();
+  }
 }
 
 void S(){
-
+  if (equals(tokenActual, ID)) {
+    eat(ID);
+    eat(EQ);
+    E();
+    eat(PCOMA);
+  }
+  // duda con
+  // S -> id = E; S | id = E;
 }
 
 void E(){
-
+  U();
+  EP();
 }
 
 void EP(){
-
+  if (equals(tokenActual, PLUS)) {
+    eat(PLUS);
+    U();
+    EP();
+  }
+  else if (equals(tokenActual, MINUS)) {
+    eat(MINUS);
+    U();
+    EP();
+  }
 }
 
 void U(){
-
+  F();
+  UP();
 }
 
 void UP(){
-
+  if (equals(tokenActual, MULT)) {
+    eat(MULT);
+    F();
+    UP();
+  }
+  else if (equals(tokenActual, DIV)) {
+    eat(DIV);
+    F();
+    UP();
+  }
+  else if (equals(tokenActual, MOD)) {
+    eat(MOD);
+    F();
+    UP();
+  }
 }
 
 void F(){
-
+  if (equals(tokenActual, PIZQ)) {
+    eat(PIZQ);
+    E();
+    eat(PDER);
+  }
+  else if (equals(tokenActual, ID)) {
+    eat(ID);
+  }
+  else if (equals(tokenActual, NUM)) {
+    eat(NUM);
+  }
 }
 
 void error(char *msg) {
